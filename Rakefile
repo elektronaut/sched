756e6bbe6bcc5f9c6668c9aa9a05b1d56fba61f2
Inge Jørgensen <inge@manualdesign.no>
2012-09-24 14:11:36 +0200

    Converted tabs to spaces

--- Rakefile
+++ Rakefile
@@ -2,41 +2,41 @@ require 'rubygems'
 require 'rake'
 
 begin
-	require 'jeweler'
-	Jeweler::Tasks.new do |gem|
-		gem.name = "sched"
-		gem.summary = %Q{SCHED* API client library for Ruby}
-		gem.description = %Q{SCHED* (http://sched.org) API client library for Ruby}
-		gem.email = "inge@manualdesign.no"
-		gem.homepage = "http://github.com/elektronaut/sched"
-		gem.authors = ["Inge Jørgensen"]
-		gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
-		gem.add_dependency "curb"
-		gem.add_dependency "fastercsv"
-	end
-	Jeweler::GemcutterTasks.new
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "sched"
+    gem.summary = %Q{SCHED* API client library for Ruby}
+    gem.description = %Q{SCHED* (http://sched.org) API client library for Ruby}
+    gem.email = "inge@manualdesign.no"
+    gem.homepage = "http://github.com/elektronaut/sched"
+    gem.authors = ["Inge Jørgensen"]
+    gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
+    gem.add_dependency "curb"
+    gem.add_dependency "fastercsv"
+  end
+  Jeweler::GemcutterTasks.new
 rescue LoadError
-	puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
+  puts "Jeweler (or a dependency) not available. Install it with: sudo gem install jeweler"
 end
 
 require 'rake/testtask'
 Rake::TestTask.new(:test) do |test|
-	test.libs << 'lib' << 'test'
-	test.pattern = 'test/**/test_*.rb'
-	test.verbose = true
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
 end
 
 begin
-	require 'rcov/rcovtask'
-	Rcov::RcovTask.new do |test|
-		test.libs << 'test'
-		test.pattern = 'test/**/test_*.rb'
-		test.verbose = true
-	end
+  require 'rcov/rcovtask'
+  Rcov::RcovTask.new do |test|
+    test.libs << 'test'
+    test.pattern = 'test/**/test_*.rb'
+    test.verbose = true
+  end
 rescue LoadError
-	task :rcov do
-		abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
-	end
+  task :rcov do
+    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
+  end
 end
 
 task :test => :check_dependencies
@@ -45,10 +45,10 @@ task :default => :test
 
 require 'rake/rdoctask'
 Rake::RDocTask.new do |rdoc|
-	version = File.exist?('VERSION') ? File.read('VERSION') : ""
+  version = File.exist?('VERSION') ? File.read('VERSION') : ""
 
-	rdoc.rdoc_dir = 'rdoc'
-	rdoc.title = "sched #{version}"
-	rdoc.rdoc_files.include('README*')
-	rdoc.rdoc_files.include('lib/**/*.rb')
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "sched #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
 end
--- lib/sched/client.rb
+++ lib/sched/client.rb
@@ -1,52 +1,52 @@
 module Sched
-	class Client
-		attr_accessor :conference, :api_key
-		def initialize(conference, api_key)
-			@conference, @api_key = conference, api_key
-		end
-		
-		def event(event_key)
-			event = Sched::Event.new(event_key, self)
-			if event.exists?
-				event = self.events.select{|e| e.event_key == event_key}.first
-			end
-			event
-		end
-		
-		def events
-			unless @events
-				results = FasterCSV.parse(request('event/list', nil, :get))
-				attributes = results.shift.map{|a| a.strip.to_sym}
-				@events = results.map do |row|
-					row_hash = {}
-					attributes.each_with_index do |a, i|
-						row_hash[a] = row[i]
-					end
-					event = Sched::Event.new(row_hash[:event_key], self).configure(row_hash)
-				end
-			end
-			@events
-		end
-		
-		def api_url
-			"http://#{@conference}.sched.org/api"
-		end
-		
-		def request(sub_url, data={}, method = :post)
-			data ||= {}
-			data.merge!({:api_key => @api_key})
-			url = "#{api_url}/#{sub_url}"
-			output = nil
-			if method == :post
-				post_fields = data.map{|key, value| Curl::PostField.content(key.to_s, value)}
-				c = Curl::Easy.http_post(url, post_fields)
-				output = c.body_str
-			elsif method == :get
-				get_attributes = data.map{|key, value| "#{key}=#{value}" }.join("&")
-				c = Curl::Easy.perform("#{url}?#{get_attributes}")
-				output = c.body_str
-			end
-			output
-		end
-	end
+  class Client
+    attr_accessor :conference, :api_key
+    def initialize(conference, api_key)
+      @conference, @api_key = conference, api_key
+    end
+    
+    def event(event_key)
+      event = Sched::Event.new(event_key, self)
+      if event.exists?
+        event = self.events.select{|e| e.event_key == event_key}.first
+      end
+      event
+    end
+    
+    def events
+      unless @events
+        results = FasterCSV.parse(request('event/list', nil, :get))
+        attributes = results.shift.map{|a| a.strip.to_sym}
+        @events = results.map do |row|
+          row_hash = {}
+          attributes.each_with_index do |a, i|
+            row_hash[a] = row[i]
+          end
+          event = Sched::Event.new(row_hash[:event_key], self).configure(row_hash)
+        end
+      end
+      @events
+    end
+    
+    def api_url
+      "http://#{@conference}.sched.org/api"
+    end
+    
+    def request(sub_url, data={}, method = :post)
+      data ||= {}
+      data.merge!({:api_key => @api_key})
+      url = "#{api_url}/#{sub_url}"
+      output = nil
+      if method == :post
+        post_fields = data.map{|key, value| Curl::PostField.content(key.to_s, value)}
+        c = Curl::Easy.http_post(url, post_fields)
+        output = c.body_str
+      elsif method == :get
+        get_attributes = data.map{|key, value| "#{key}=#{value}" }.join("&")
+        c = Curl::Easy.perform("#{url}?#{get_attributes}")
+        output = c.body_str
+      end
+      output
+    end
+  end
 end
